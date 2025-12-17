2fa8697def6b1f6a745de34092bb0b258131b152
Fayaz <kfayaz1407@gmail.com>
2025-12-17 17:51:44 +0530

    cors error fix

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cors({
     origin:[
         'http://localhost:5173',
         'http://localhost:3000',
-        "https://rag-news-client.vercel.app/"
+        "https://rag-news-client.vercel.app"
     ]
 }));
 
